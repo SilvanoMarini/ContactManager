91e2a4bf966c324b646d64d0102e6ea9b26694a5
Silvano Junior <silvano.marini.dev@gmail.com>
2026-02-11 22:41:31 -0300

    feat(contacts): introduce paginated contact listing with reusable pagination component
    Refactor templates to use page_obj and add pagination navigation
    with query string persistence for search results.

--- contact_manager/contacts/views/contacts_views.py
+++ contact_manager/contacts/views/contacts_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
 from contacts.models import Contact
 
 
@@ -8,8 +9,12 @@ def index(request):
         .filter(show=True)\
         .order_by('first_name', 'last_name')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contacts | '
     }
 
@@ -53,8 +58,12 @@ def search(request):
                 )\
         .order_by('first_name', 'last_name')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': f'Search: {searched_contact} | ',
         'searched_contact': searched_contact
     }
